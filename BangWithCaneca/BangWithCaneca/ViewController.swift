277ea659aff0af2733b03bf5ed6159658fb49110
franclinc <fcmo@cin.ufpe.br>
2015-08-14 08:44:12 -0300

    criação de projeto

---
+++ BangWithCaneca/BangWithCaneca/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BangWithCaneca
+//
+//  Created by Franclin Cabral on 14/08/15.
+//  Copyright (c) 2015 Franclin Cabral. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
